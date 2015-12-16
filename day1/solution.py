d08da0aca5d6f8a0ebd8623c02338ea4c24e1d57
John Cline <jocline@ebay.com>
2015-12-16 11:42:57 -0500

    solution for day 1 part 2

--- day1/solution.py
+++ day1/solution.py
@@ -1,6 +1,7 @@
 import sys
 
 instructions = ''
+
 if len(sys.argv) > 2 and sys.argv[1] == '-f':
   f = open(sys.argv[2], 'r')
   instructions = f.read()
@@ -8,7 +9,22 @@ if len(sys.argv) > 2 and sys.argv[1] == '-f':
 else:
   instructions = sys.argv[1]
 
-num_up = instructions.count('(')
-num_down = instructions.count(')')
+current_floor = 0
+first_basement = -1
+num_up = 0
+num_down = 0
+
+for i in range(len(instructions)):
+  c = instructions[i]
+  if c == '(':
+    current_floor = current_floor + 1
+    num_up = num_up + 1
+  elif c == ')':
+    current_floor = current_floor - 1
+    num_down = num_down + 1
+  if (current_floor < 0) and (first_basement < 0):
+    first_basement = i + 1
+
 print('Instructions: ' + str(num_up) + " up, " + str(num_down) + " down")
-print('Current floor: ' + str(num_up - num_down))
+print('Current floor: ' + str(current_floor))
+print('First basement: ' + str(first_basement))
