602f337117a3a069efcbb8343edfe79945e92958
John Cline <jocline@ebay.com>
2015-12-16 12:10:47 -0500

    code review suggestions
    thanks to @alexclare and @paulvstheworld

--- day1/solution.py
+++ day1/solution.py
@@ -1,30 +1,31 @@
 import sys
 
-instructions = ''
 
-if len(sys.argv) > 2 and sys.argv[1] == '-f':
-  f = open(sys.argv[2], 'r')
-  instructions = f.read()
-  f.close()
-else:
-  instructions = sys.argv[1]
+if __name__ == '__main__':
+  
+  instructions = ''
 
-current_floor = 0
-first_basement = -1
-num_up = 0
-num_down = 0
+  if len(sys.argv) > 2 and sys.argv[1] == '-f':
+    with open(sys.argv[2], 'r') as f:
+      instructions = f.read()
+  else:
+    instructions = sys.argv[1]
 
-for i in range(len(instructions)):
-  c = instructions[i]
-  if c == '(':
-    current_floor = current_floor + 1
-    num_up = num_up + 1
-  elif c == ')':
-    current_floor = current_floor - 1
-    num_down = num_down + 1
-  if (current_floor < 0) and (first_basement < 0):
-    first_basement = i + 1
+  current_floor = 0
+  first_basement = -1
+  num_up = 0
+  num_down = 0
 
-print('Instructions: ' + str(num_up) + " up, " + str(num_down) + " down")
-print('Current floor: ' + str(current_floor))
-print('First basement: ' + str(first_basement))
+  for i,c in enumerate(instructions):
+    if c == '(':
+      current_floor = current_floor + 1
+      num_up = num_up + 1
+    elif c == ')':
+      current_floor = current_floor - 1
+      num_down = num_down + 1
+    if (current_floor < 0) and (first_basement < 0):
+      first_basement = i + 1
+
+  print('Instructions: {} up, {} down'.format(num_up, num_down))
+  print('Current floor: {}'.format(current_floor))
+  print('First basement: {}'.format(first_basement))
