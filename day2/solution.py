12e07ceea7f2b280a2d01225705b40af74afa6ab
John Cline <jocline@ebay.com>
2015-12-17 12:11:42 -0500

    solution to day 2 part 2

--- day2/solution.py
+++ day2/solution.py
@@ -14,13 +14,18 @@ if __name__ == '__main__':
   else:
     dimension_list = sys.argv[1]
 
-  total = 0
+  total_paper = 0
+  total_ribbon = 0
 
   for package in dimension_list.splitlines():
     package_sizes = map(int, package.split('x'))
     surface_area = getSurfaceArea(package_sizes)
+    volume = package_sizes[0] * package_sizes[1] * package_sizes[2]
     package_sizes.remove(max(package_sizes))
     extra = package_sizes[0] * package_sizes[1]
-    total = total + surface_area + extra
+    extra_perimeter = 2*package_sizes[0] + 2*package_sizes[1]
+    total_paper = total_paper + surface_area + extra
+    total_ribbon = total_ribbon + extra_perimeter + volume
 
-  print('Total: {}'.format(total))
+  print('Total Paper: {}'.format(total_paper))
+  print('Total Ribbon: {}'.format(total_ribbon))
